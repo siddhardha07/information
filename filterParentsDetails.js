1894120f32732b874dc5c6f2da95493f6efdf125
Siddhardha Gunupuru <ideasbag31@gmail.com>
2024-12-20 11:12:50 +0530

    added filterAbove30.js

---
+++ filterParentsDetails.js
@@ -0,0 +1,25 @@
+const personAndGuardianDetails = [
+  ["Aarav", "9876543210", 25, "123 MG Road, Mumbai", ["9123456780", "Suresh"]],
+  ["Ananya", "8765432109", 35, "456 Brigade Road, Bengaluru", ["8223456781", "Lakshmi"]],
+  ["Ishaan", "7654321098", 19, "789 Park Street, Kolkata", ["8323456782", "Ramesh"]],
+  ["Meera", "6543210987", 42, "101 Connaught Place, Delhi", ["8423456783", "Sunita"]],
+  ["Rohan", "5432109876", 28, "202 Marine Drive, Chennai", ["8523456784", "Arun"]],
+  ["Priya", "4321098765", 31, "303 Sarjapur Road, Hyderabad", ["8623456785", "Geeta"]],
+  ["Kavya", "3210987654", 18, "404 Carter Road, Pune", ["8723456786", "Rajesh"]],
+  ["Aditya", "2109876543", 45, "505 Residency Road, Ahmedabad", ["8823456787", "Vimala"]],
+  ["Naina", "1098765432", 23, "606 MG Road, Jaipur", ["8923456788", "Ashok"]],
+  ["Veer", "0198765431", 37, "707 Lodhi Road, Chandigarh", ["9023456789", "Manju"]]
+]
+
+const above30 = function (personDetails) {
+  return personDetails[2] > 30;
+}
+
+const nameAndPhoneNumber = function (personDetails) {
+  const guardianDetails = [personDetails[4][1], personDetails[4][0]];
+  return [personDetails[0], personDetails[1], guardianDetails].flat();
+}
+
+const filterPeopleAbove30 = function (peopleDetails) {
+  return peopleDetails.filter(above30).map(nameAndPhoneNumber);
+}
